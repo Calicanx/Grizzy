6ecadd0a1133dcf81db1256ca896e8de933085fd
0xSc0rpi0n <55562628+Calicanx@users.noreply.github.com>
2023-08-09 19:05:28 +0300

    Add files via upload

---
+++ src/Grizzy/Grizzy.jsx
@@ -0,0 +1,28 @@
+/* Code generated with AutoHTML Plugin for Figma */
+import "./Grizzy.css";
+
+export const Grizzy = ({ ...props }) => {
+  return (
+    <div className="grizzy">
+      <img className="logo-2" src="logo-2.png" />
+      <img className="logo" src="logo.png" />
+      <div className="frame-229">
+        <div className="frame-227">
+          <input type="text" placeholder="Podcast name" className="frame-224"/>
+          <input type="text" placeholder="Discussion title" className="frame-225"/>
+          <div className="frame-223">
+            <img className="rectangle-125" src="rectangle-125.png" />
+            <div className="add-participants">Add participants </div>
+          </div>
+          <div className="frame-226">
+            <img className="rectangle-125" src="rectangle-125.png" />
+            <div className="add-sponsors">Add sponsors </div>
+          </div>
+        </div>
+        <div className="frame-228">
+          <div className="submit">Submit </div>
+        </div>
+      </div>
+    </div>
+  );
+};
